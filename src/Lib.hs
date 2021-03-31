1ee71f1115ffa107ee3aabc9cd1c5271490efd32
Providence Salumu <Providence.Salumu@smunix.com>
2021-03-31 03:06:42 -0400

    add polygon with trails

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,13 +13,13 @@ import Graphics.Dynamic.Plot.R2
 
 diag = circles ||| strutX 1 ||| (circles <> square 1)
   where
-    d1 = juxtapose unitX (square 1) (circle 1 & fc red)
-    d2 = juxtapose (unitX ^+^ unitY) (square 1) (circle 1 & fc green)
-    d3 = juxtapose unitY (square 1) (circle 1 & fc blue)
+    d1 = juxtapose unitX (square 1) (circle 1 # fc red)
+    d2 = juxtapose (unitX ^+^ unitY) (square 1) (circle 1 # fc green)
+    d3 = juxtapose unitY (square 1) (circle 1 # fc blue)
     circles = mconcat [d1, d2, d3]
 
 diag' :: Int -> PlainGraphicsR2
-diag' n = hrule (2 * sum sizes) === circles & centerX
+diag' n = hrule (2 * sum sizes) === circles # centerX
   where
     arrOpts =
       with
@@ -35,9 +35,9 @@ diag' n = hrule (2 * sum sizes) === circles & centerX
       sizes
         & zip [1 .. n]
         <&> ( \k@(sz, i) ->
-                -- text (show k & fontSizeL 0.2 & fc white)
-                --   <>
-              circle sz & fc green & named i
+                text (show k # fontSizeL 0.2 # fc white)
+                  <>
+              circle sz # fc green # named i
             )
         <&> alignT
         & atPoints (regPoly n 10 & trailVertices)
